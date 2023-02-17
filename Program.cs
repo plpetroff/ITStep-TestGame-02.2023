5039c9a07c2c9a57cb8886f2e1371c54dcb2982b
Pl. Petroff <plpetroff@gmail.com>
2023-02-17 11:06:22 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int inputSize = int.Parse(Console.ReadLine());
+double[,] matrix = new double[inputSize, inputSize];
+//int flag = 0;
+
+for (int row = 0; row < inputSize; row++)
+{
+    for (int col = 0; col < inputSize; col++)
+    {
+        matrix[row, col] = Math.Pow(2, (col + row));
+        Console.Write(matrix[row, col] + " ");
+    }
+    Console.WriteLine();
+}
+
+double sum = 0;
+for (int r = 0; r < inputSize; r++)
+{
+    for (int c = 0; c < inputSize; c++)
+    {
+        if (c > r)
+        {
+            sum += matrix[r, c];
+        }
+
+    }
+}
+Console.WriteLine(sum);
+
